163dae6a059da78a1f25c00169593bca79513421
For Something <134180518+ForSomething2410@users.noreply.github.com>
2024-10-13 03:44:28 +0530

    Add files via upload

--- finpulse.js
+++ finpulse.js
@@ -24,7 +24,7 @@ function dC() {
             let name = document.getElementById("acNa").innerText;
             document.cookie = "finpulseUser" + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
             alert("loged out scuessfully.");
-           location.href="login.html";
+            location.href="login.html";
         }
 
 let preNo = "";
